c74c2dc5f178c0fbe14d30e000badd75d371d83e
DCI Student <student@example.com>
2024-10-25 02:32:28 +0200

    navbar ist super

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 export function loadHeader() {
-    const header = document.getElementById("header");
-    header.innerHTML = `
+  const header = document.getElementById("header");
+  header.innerHTML = `
       <header>
           <div class="kuhlturm-header">
               <div class="kuhlturm-header-top">
@@ -23,23 +23,32 @@ export function loadHeader() {
                           <a href="#" class="kuhlturm-nav-button" id="Unternehmen">
                               <i class="fas fa-briefcase"></i> Unternehmen
                           </a>
-                          <div class="kuhlturm-dropdown-content">
+                          <div class="kuhlturm-dropdown-content ">
                               <div class="kuhlturm-dropdown-row">
                                   <!-- Leitbild und Geschäftsprinzipien -->
-                                  <div class="kuhlturm-dropdown-column">
+                                  <div class="kuhlturm-dropdown-column unternehmen-dropdown">
                                       <ul>
-                                          <li><a href="#" class="upper-level">Leitbild und Geschäftsprinzipien</a>
+                                          <li><a href="#" class="upper-level">Über Uns</a>
                                               <ul>
-                                                  <li><a href="#">Alt Link 1</a></li>
-                                                  <li><a href="#">Alt Link 2</a></li>
-                                                  <li><a href="#">Alt Link 3</a></li>
+                                                  <li><a href="#">Vision und Mission</a></li>   
+                                                  <li><a href="#">Zertifikate</a></li>
+                                                  <li><a href="#">Referenzen</a></li>
+                                                  <li><a href="#">Kontakt</a></li>
+                                                  <li><a href="#">Galeri</a></li>
+
                                               </ul>
                                           </li>
                                           <li><a href="#" class="upper-level">Organisation</a>
                                               <ul>
-                                                  <li><a href="#">Alt Link 1</a></li>
-                                                  <li><a href="#">Alt Link 2</a></li>
-                                                  <li><a href="#">Alt Link 3</a></li>
+                                                  <li><a href="#">Unsere Aktivitäten</a></li>
+                                                  <li><a href="#">Unser Team</a></li>
+                                                  <li><a href="#">Unsere Geschichte</a></li>
+                                              </ul>
+                                          </li>
+                                          <li><a href="#" class="upper-level">Neuigkeiten</a>
+                                              <ul>
+                                                  <li><a href="#">Last Neuigkeiten</a></li>
+                                                  <li><a href="#">Arsiv</a></li>
                                               </ul>
                                           </li>
                                       </ul>
@@ -47,28 +56,49 @@ export function loadHeader() {
                               </div>
                           </div>
                       </li>
-                      <!-- Kernenergie Menüsü -->
+                      <!-- Bibliothek Menüsü -->
                       <li class="kuhlturm-dropdown">
-                          <a href="#" class="kuhlturm-nav-button" id="Kernenergie">
-                              <i class="fas fa-atom"></i> Kernenergie
+                          <a href="#" class="kuhlturm-nav-button" id="Bibliothek">
+                              <i class="fas fa-atom"></i> Bibliothek
                           </a>
                           <div class="kuhlturm-dropdown-content">
                               <div class="kuhlturm-dropdown-row">
-                                  <!-- Stromproduktion -->
-                                  <div class="kuhlturm-dropdown-column">
+                                  <!-- Bibliotek -->
+                                  <div class="kuhlturm-dropdown-column bibliothek-dropdown">
                                       <ul>
-                                          <li><a href="#" class="upper-level">Stromproduktion</a>
+                                          <li><a href="#" class="upper-level">Artikel</a>
                                               <ul>
-                                                  <li><a href="#">Alt Link 1</a></li>
-                                                  <li><a href="#">Alt Link 2</a></li>
-                                                  <li><a href="#">Alt Link 3</a></li>
+                                                  <li><a href="#">Was ist eine Wasserkühlanlage?</a></li>
+                                                  <li><a href="#">Eigenschaften der Ensotek Kühltürme</a></li>
+                                                  <li><a href="#">Erforderliche Informationen für die Auswahl eines Kühlturms</a></li>
+                                                  <li><a href="#">Offener Kühlkreislaufturm Funktionsprinzip</a></li>
+                                                  <li><a href="#">Funktionsprinzip des geschlossenen Kreislauf-Kühlturms</a></li>
+                                                  <li><a href="#">Feuchtkugeltemperatur Rechner</a></li>
+                                                  <li><a href="#">Feuchtkugeltemperatur in nach Städten</a></li>
                                               </ul>
                                           </li>
-                                          <li><a href="#" class="upper-level">Funktion des Kernkraftwerks</a>
+                                          <li><a href="#" class="upper-level">Produkte</a>
                                               <ul>
-                                                  <li><a href="#">Alt Link 1</a></li>
-                                                  <li><a href="#">Alt Link 2</a></li>
-                                                  <li><a href="#">Alt Link 3</a></li>
+                                                  <li><a href="#">Kühlturm Mit Offenem Kreislauf</a></li>
+                                                  <li><a href="#">Geschlossener Kreislaufkühlturm</a></li>
+                                                  <li><a href="#">Service und Wartung</a></li>
+                                                  <li><a href="#">Mietkühltürme</a></li>
+                                                  <li><a href="#">Verdunstungskondensator</a></li>
+                                                  <li><a href="#">Kühlturmwasseraufbereitung</a></li>
+                                              </ul>
+                                          </li>
+                                          <li><a href="#" class="upper-level">Turmmaterialien</a>
+                                              <ul>
+                                                  <li><a href="#">Wasserkühlturmmotor Getriebe Lüfter</a></li>
+                                                  <li><a href="#">Axialventilator (Lüfter) für Kühltürme</a></li>
+                                                  <li><a href="#">Vibrationsschalter</a></li>
+                                                  <li><a href="#">Drift Eliminator</a></li>
+                                                  <li><a href="#">Wasserverteilungssystem</a></li>
+                                                  <li><a href="#">Kühlturmdüse</a></li>
+                                                  <li><a href="#">Servicefenster</a></li>
+                                                  <li><a href="#">Spritzgitter Kühlturmfüllung</a></li>
+                                                  <li><a href="#">Wasserkühlturmpumpe</a></li>
+                                                  <li><a href="#">Plattenwärmetauscher</a></li>
                                               </ul>
                                           </li>
                                       </ul>
@@ -78,32 +108,41 @@ export function loadHeader() {
                       </li>
                       <!-- Besucherangebote Menüsü -->
                       <li class="kuhlturm-dropdown">
-                          <a href="#" class="kuhlturm-nav-button" id="Besucherangebote">
-                              <i class="fas fa-users"></i> Besucherangebote
+                          <a href="#" class="kuhlturm-nav-button" id="Hilfmittel">
+                              <i class="fas fa-users"></i>Hilfmittel
                           </a>
                           <div class="kuhlturm-dropdown-content">
                               <div class="kuhlturm-dropdown-row">
                                   <!-- Infozentrum -->
-                                  <div class="kuhlturm-dropdown-column">
+                                  <div class="kuhlturm-dropdown-column hilfmittel-dropdown">
                                       <ul>
-                                          <li><a href="#" class="upper-level">Infozentrum</a>
+                                          <li><a href="#" class="upper-level">Hilfmittel</a>
                                               <ul>
-                                                  <li><a href="#">Alt Link 1</a></li>
-                                                  <li><a href="#">Alt Link 2</a></li>
-                                                  <li><a href="#">Alt Link 3</a></li>
+                                                  <li><a href="#">FAQ</a></li>
+                                                  <li><a href="#">Glossar der Kühltechnologien</a></li>
+                                                  <li><a href="#">Technische Artikel</a></li>
                                               </ul>
                                           </li>
-                                          <li><a href="#" class="upper-level">Seminarräume</a>
+                                          <li><a href="#" class="upper-level">Reservierter Bereich</a>
                                               <ul>
-                                                  <li><a href="#">Alt Link 1</a></li>
-                                                  <li><a href="#">Alt Link 2</a></li>
-                                                  <li><a href="#">Alt Link 3</a></li>
+                                                  <li><a href="#">Bewerbung</a></li>
+                                                  <li><a href="#">Partnerschaft</a></li>
+                                                  <li><a href="#">Ein Angebot bekommen</a></li>
+                                                  <li><a href="#">Nach Informationen fragen</a></li>
+                                              </ul>
+                                          </li>
+                                           <li><a href="#" class="upper-level">Downloads</a>
+                                              <ul>
+                                                  <li><a href="#">Broschüren</a></li>
+                                                  <li><a href="#">Bilddatenbank</a></li>
                                               </ul>
                                           </li>
                                       </ul>
                                   </div>
                               </div>
                           </div>
+
+
                       </li>
                   </ul>
               </nav>
@@ -111,54 +150,56 @@ export function loadHeader() {
       </header>
       `;
 
-    // FontAwesome ikonu kullanabilmek için kütüphaneyi dahil et
-    const fontAwesomeLink = document.createElement("link");
-    fontAwesomeLink.rel = "stylesheet";
-    fontAwesomeLink.href =
-      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css";
-    document.head.appendChild(fontAwesomeLink);
+  // FontAwesome ikonu kullanabilmek için kütüphaneyi dahil et
+  const fontAwesomeLink = document.createElement("link");
+  fontAwesomeLink.rel = "stylesheet";
+  fontAwesomeLink.href =
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css";
+  document.head.appendChild(fontAwesomeLink);
 
-    // Dropdown menülerin başlangıçta kapalı olması için
-    const dropdowns = document.querySelectorAll(".kuhlturm-dropdown-content");
-    dropdowns.forEach((dropdown) => {
-      dropdown.classList.remove("kuhlturm-show"); // Başlangıçta kapalı olacak
+  // Dropdown menülerin başlangıçta kapalı olması için
+  const dropdowns = document.querySelectorAll(".kuhlturm-dropdown-content");
+  dropdowns.forEach((dropdown) => {
+    dropdown.classList.remove("kuhlturm-show"); // Başlangıçta kapalı olacak
+  });
+
+  // Dropdown menü açma/kapama
+  const dropdownItems = document.querySelectorAll(".kuhlturm-dropdown");
+  dropdownItems.forEach((dropdown) => {
+    const button = dropdown.querySelector(".kuhlturm-nav-button");
+    const content = dropdown.querySelector(".kuhlturm-dropdown-content");
+
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      dropdownItems.forEach((otherDropdown) => {
+        if (otherDropdown !== dropdown) {
+          otherDropdown
+            .querySelector(".kuhlturm-dropdown-content")
+            .classList.remove("kuhlturm-show");
+        }
+      });
+      content.classList.toggle("kuhlturm-show");
     });
+  });
 
-    // Dropdown menü açma/kapama
-    const dropdownItems = document.querySelectorAll(".kuhlturm-dropdown");
-    dropdownItems.forEach((dropdown) => {
-        const button = dropdown.querySelector(".kuhlturm-nav-button");
-        const content = dropdown.querySelector(".kuhlturm-dropdown-content");
+  // Mobil menü açma/kapatma işlevselliği ve animasyonlar
+  const menuToggle = document.getElementById("menu-toggle");
+  const mainNav = document.getElementById("main-nav");
+  const bars = document.querySelectorAll(".bar");
 
-        button.addEventListener("click", (event) => {
-            event.preventDefault();
-            dropdownItems.forEach((otherDropdown) => {
-                if (otherDropdown !== dropdown) {
-                    otherDropdown.querySelector(".kuhlturm-dropdown-content").classList.remove("kuhlturm-show");
-                }
-            });
-            content.classList.toggle("kuhlturm-show");
-        });
+  menuToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("kuhlturm-mobile-visible");
+    menuToggle.classList.toggle("open");
+
+    // Hamburger menünün çubuklarının animasyonu
+    bars.forEach((bar, index) => {
+      if (index === 0) {
+        bar.classList.toggle("rotate-down");
+      } else if (index === 1) {
+        bar.classList.toggle("hide");
+      } else if (index === 2) {
+        bar.classList.toggle("rotate-up");
+      }
     });
-
-    // Mobil menü açma/kapatma işlevselliği ve animasyonlar
-    const menuToggle = document.getElementById("menu-toggle");
-    const mainNav = document.getElementById("main-nav");
-    const bars = document.querySelectorAll('.bar');
-
-    menuToggle.addEventListener("click", () => {
-        mainNav.classList.toggle("kuhlturm-mobile-visible");
-        menuToggle.classList.toggle("open");
-
-        // Hamburger menünün çubuklarının animasyonu
-        bars.forEach((bar, index) => {
-            if (index === 0) {
-                bar.classList.toggle("rotate-down");
-            } else if (index === 1) {
-                bar.classList.toggle("hide");
-            } else if (index === 2) {
-                bar.classList.toggle("rotate-up");
-            }
-        });
-    });
+  });
 }
--- contact.php
+++ contact.php
@@ -1,23 +1,36 @@
 <?php
 // Formdan gelen verileri al
-$name = $_POST['name'];
+$vorname = $_POST['vorname'];
+$nachname = $_POST['nachname'];
+$firma = $_POST['firma'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$postleitzahl = $_POST['postleitzahl'];
+$berufsbezeichnung = $_POST['berufsbezeichnung'];
+$markttyp = $_POST['markttyp'];
+$stelle = $_POST['stelle'];
+$tower_seriennummer = $_POST['tower_seriennummer'];
+$nachricht = $_POST['nachricht'];
 
 // E-posta ayarları
 $to = "orhanguzell@gmail.com"; // E-posta gönderilecek adres
+$subject = "Neue Anfrage von Website"; // E-posta başlığı
 $headers = "From: " . $email . "\r\n";
 $headers .= "Reply-To: " . $email . "\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 // E-posta içeriği
 $email_content = "
-    <h2>Neue Nachricht</h2>
-    <p><strong>Name:</strong> {$name}</p>
+    <h2>Neue Anfrage</h2>
+    <p><strong>Vorname:</strong> {$vorname}</p>
+    <p><strong>Nachname:</strong> {$nachname}</p>
+    <p><strong>Firma:</strong> {$firma}</p>
     <p><strong>E-Mail:</strong> {$email}</p>
-    <p><strong>Betreff:</strong> {$subject}</p>
-    <p><strong>Nachricht:</strong><br>{$message}</p>
+    <p><strong>Postleitzahl:</strong> {$postleitzahl}</p>
+    <p><strong>Berufsbezeichnung:</strong> {$berufsbezeichnung}</p>
+    <p><strong>Markttyp:</strong> {$markttyp}</p>
+    <p><strong>Stelle:</strong> {$stelle}</p>
+    <p><strong>Turm Seriennummer:</strong> {$tower_seriennummer}</p>
+    <p><strong>Nachricht:</strong><br>{$nachricht}</p>
 ";
 
 // E-posta gönderme fonksiyonu
