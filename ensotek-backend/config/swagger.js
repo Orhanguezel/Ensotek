df01faca7f6c41dafa759be85721cf5cb4a58897
Orhan Güzel <orhan.guezel@dci-student.org>
2025-01-03 05:48:29 +0100

    ok

--- ensotek-backend/config/swagger.js
+++ ensotek-backend/config/swagger.js
@@ -1,29 +1,32 @@
-const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerJsDoc = require("swagger-jsdoc");
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || "development";
 const SERVER_URL =
-    NODE_ENV === 'development'
-        ? 'http://localhost:5004' // Geliştirme ortamı URL'si
-        : 'https://www.ensotek.de'; // Üretim ortamı URL'si
+  NODE_ENV === "development"
+    ? "http://localhost:5004" // Geliştirme ortamı URL'si
+    : "https://www.ensotek.de"; // Üretim ortamı URL'si
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Ensotek API',
-            version: '1.0.0',
-            description: 'Ensotek API documentation',
-        },
-        servers: [
-            {
-                url: SERVER_URL, // Sunucu URL'si
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Ensotek API",
+      version: "1.0.0",
+      description: "Ensotek API documentation",
     },
-    apis: ['./routes/*.js'], // Swagger açıklamaları için dosya yolu
+    servers: [
+      {
+        url: "https://www.ensotek.de/api",
+        description: "Production server",
+      },
+      {
+        url: "http://localhost:5004",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"], // Swagger açıklamaları için dosya yolu
 };
 
 const swaggerSpecs = swaggerJsDoc(swaggerOptions);
 module.exports = swaggerSpecs;
-
-
