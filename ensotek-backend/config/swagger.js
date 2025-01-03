c4251a1b2ca2aa3aa0e51c45cf31d067d0c36355
Orhan Güzel <orhan.guezel@dci-student.org>
2025-01-03 05:39:09 +0100

    ok

--- ensotek-backend/config/swagger.js
+++ ensotek-backend/config/swagger.js
@@ -3,12 +3,12 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const SERVER_URL =
     NODE_ENV === 'development'
-        ? 'http://localhost:5004' // Development URL
-        : 'https://www.ensotek.de'; // Production URL
+        ? 'http://localhost:5004' // Geliştirme ortamı URL'si
+        : 'https://www.ensotek.de'; // Üretim ortamı URL'si
 
 const swaggerOptions = {
     definition: {
-        openapi: '3.0.0', // OpenAPI sürümü
+        openapi: '3.0.0',
         info: {
             title: 'Ensotek API',
             version: '1.0.0',
@@ -20,9 +20,10 @@ const swaggerOptions = {
             },
         ],
     },
-    apis: ['./routes/*.js'], // Swagger açıklamalarını içeren dosyalar
+    apis: ['./routes/*.js'], // Swagger açıklamaları için dosya yolu
 };
 
 const swaggerSpecs = swaggerJsDoc(swaggerOptions);
 module.exports = swaggerSpecs;
+
 
